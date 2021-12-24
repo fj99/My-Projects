8341df8e79d116db9b275dad68dd31d8f51cb99d
fj99 <68979865+fj99@users.noreply.github.com>
2021-12-23 21:11:02 -0500

    adding my pages

--- Daycare System/index.php
+++ Daycare System/index.php
@@ -226,9 +226,10 @@
                 <div class="row align-items-center">
                     <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2021</div>
                     <div class="col-lg-4 my-3 my-lg-0">
-                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        <!-- <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a> -->
+                        <a class="btn btn-dark btn-social mx-2" href="https://github.com/fj99?tab=repositories"><i class="fab fa-github"></i></a> 
+                        <a class="btn btn-dark btn-social mx-2" href="https://www.linkedin.com/in/felix-fernandez-92a0021b5/"><i class="fab fa-linkedin-in"></i></a>
                     </div>
                     <div class="col-lg-4 text-lg-end">
                         <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
