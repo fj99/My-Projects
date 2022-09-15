2bc59063583ee1aae168f9e88e57014adb1c4f0a
Felix Fernandez <fj99.fernandez@gmail.com>
2022-09-15 09:39:46 -0400

    fixing 360-tour

--- 360-tour/index.js
+++ 360-tour/index.js
@@ -389,44 +389,4 @@
   // Display the initial scene.
   switchScene(scenes[0]);
 
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-
-    return null;
-  }
-
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
 })();
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-
-  return null;
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
