be0bd248287708e558cdcbba3e3131dda4048b4f
Felix Fernandez <fj99.fernandez@gmail.com>
2022-10-26 14:29:08 -0400

    bug

--- Website/assets/js/app.js
+++ Website/assets/js/app.js
@@ -1,14 +1,14 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry)
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show')
-    }
-  });
-});
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry)
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show');
+//     } else {
+//       entry.target.classList.remove('show')
+//     }
+//   });
+// });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll('.gone');
+// hiddenElements.forEach((el) => observer.observe(el));
 
