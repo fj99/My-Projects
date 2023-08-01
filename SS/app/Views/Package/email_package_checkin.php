c6b2ef4c3dc8297a30a291f02bcdd2a315b3d6f1
Felix Fernandez <fj99.fernandez@gmail.com>
2023-08-01 10:48:12 -0400

    cleaning

--- SS/app/Views/Package/email_package_checkin.php
+++
@@ -1,9 +0,0 @@
-Hello <?php echo $name; ?>,
-
-A package has arrived for you in <?php echo $hall; ?> from the carrier: <?php echo $carrier; ?>.
-The tracking number is: <?php echo $tracking_num; ?>.
-
-Don't forget to pick up your package at the front desk of your residence hall during the hours the desk is staffed.
-
-Residence Life
-Southern Connecticut State University
